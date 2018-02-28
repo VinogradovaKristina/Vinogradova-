4245919156cdce089e802d49af258b78757ab355
KristinaVinogradova <krectik22@mail.ru>
2018-02-28 11:08:14 +0300

    Ист Ть 21 Виноградова Кристина Лабораторная 2

---
+++ VinogradovaLab2/VinogradovaLab2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinogradovaLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double r = 0, x, y;
+            Console.WriteLine("Упражнение №1");
+            Console.Write("x=");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("r=");
+            r = Convert.ToDouble(Console.ReadLine());
+            if (-3 <= x && x < -2)
+                y = -x - 2;
+            else if (-2 <= x && x < -1)
+                y = (1 - (x + 1));
+            else if (-1 <= x && x < 1)
+                y = 1;
+            else if (1 <= x && x < 2)
+                y = -2 * x + 3;
+            else if (2 <= x && x <= 5)
+                y = 1;
+            else
+                Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
